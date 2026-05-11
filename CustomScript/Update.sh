6b4b6f5822e3ed81e69925d0dd1e945b49a1c616
PixelPulsea <achka.bt@gmail.com>
2026-05-11 15:11:14 +0800

    readability

--- CustomScript/Update.sh
+++ CustomScript/Update.sh
@@ -1,7 +1,10 @@
-sudo pacman -Syyu
+sudo pacman -Syyu --noconfirm
+
 journalctl --vacuum-time=365d
+
 sudo pacman -Rs $(pacman -Qdtq) --noconfirm
 sudo pacman -Scc --noconfirm
 sudo rm -rf /var/tmp/*
+
 flatpak update --noninteractive
 flatpak remove --unused -y
