6627ddd03755b5b027b77f725f0ae1a5e9034b02
PixelPulsea <achka.bt@gmail.com>
2026-05-23 17:00:00 +0800

    Custom Installer

--- CustomScript/GitCloning.sh
+++ CustomScript/GitCloning.sh
@@ -1,9 +1,40 @@
 #! /usr/bin/env bash
 
 USERNAME="PixelPulsea"
-echo "Enter the name of the Github Repo:"
-read NAME
 
-git clone git@github.com:$USERNAME/$NAME.git
-cd $NAME
-direnv allow
+echo "Is it from your own repos? [y/n]"
+read CUSTOM
+
+if [ $CUSTOM == "y" ]; then
+	echo "Enter the name of the Github Repo"
+	read NAME
+	git clone git@github.com:$USERNAME/$NAME.git
+	cd $NAME
+	direnv allow
+elif [ $CUSTOM == "n" ]; then
+	echo "Enter the name of the repo from the AUR"
+	read URL
+	git clone https://aur.archlinux.org/$URL.git
+
+	cd $URL
+	echo "Do you want to install this? [y/n]"
+	read confirmation
+
+	if [ $confirmation == "y" ]; then
+		echo "===================PKGBUILD==================="
+		cat PKGBUILD
+		echo "=============================================="
+		echo "Final confirmation. Do you want to install this? [y/n]"
+		read finalconf
+
+		if [ $finalconf == "y" ]; then
+			makepkg -sic
+		else
+			echo "Install Aborted"
+		fi
+	else
+		echo "Install Aborted"
+	fi
+else
+	echo "ERROR"
+fi
