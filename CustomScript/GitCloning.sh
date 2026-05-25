5823d20c7b0ff791099099a49393b8306e16626e
PixelPulsea <achka.bt@gmail.com>
2026-05-25 21:38:09 +0800

    double > single

--- CustomScript/GitCloning.sh
+++ CustomScript/GitCloning.sh
@@ -5,13 +5,13 @@ USERNAME="PixelPulsea"
 echo "Is it from your own repos? [y/n]"
 read CUSTOM
 
-if [ $CUSTOM == "y" ]; then
+if [[ $CUSTOM == "y" ]]; then
 	echo "Enter the name of the Github Repo"
 	read NAME
 	git clone git@github.com:$USERNAME/$NAME.git
 	cd $NAME
 	direnv allow
-elif [ $CUSTOM == "n" ]; then
+elif [[ $CUSTOM == "n" ]]; then
 	echo "Enter the name of the repo from the AUR"
 	read URL
 	git clone https://aur.archlinux.org/$URL.git
@@ -20,21 +20,21 @@ elif [ $CUSTOM == "n" ]; then
 	echo "Do you want to install this? [y/n]"
 	read confirmation
 
-	if [ $confirmation == "y" ]; then
+	if [[ $confirmation == "y" ]]; then
 		echo "===================PKGBUILD==================="
 		cat PKGBUILD
 		echo "=============================================="
 		echo "Final confirmation. Do you want to install this? [y/n]"
 		read finalconf
 
-		if [ $finalconf == "y" ]; then
+		if [[ $finalconf == "y" ]]; then
 			makepkg -sic
 			
 			echo "=============================================="
 			echo "Remove the source folder? [y/n]"
 			read afterinstall
 
-			if [ $afterinstall == "y" ]; then
+			if [[ $afterinstall == "y" || $afterinstall == "" || $afterinstall == " " ]]; then
 				cd ..
 				rm -rf $URL/
 				echo -e "\e[34mInstall Completed!\e[0m"
