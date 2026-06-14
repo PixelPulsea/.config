eb555821b5b4641541783c01c6e90ecef58923a9
PixelPulse <achka.bt@gmail.com>
2026-06-14 15:09:25 +0800

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -20,11 +20,6 @@
 		extraPackages = with pkgs; [ vpl-gpu-rt ];
 	};
 
-	environment.sessionVariables = {
-		WLR_NO_HARDWARE_CURSORS = "1";
-		NIXOS_OZONE_WL = "1";
-	};
-
 	hardware.bluetooth.enable = true;
 	services.blueman.enable = true;
 
@@ -32,22 +27,17 @@
 
 	services.pipewire = {
 		enable = true;
-		pulse.enable = true; # For PulseAudio compatibility
-			alsa.enable = true;
+		pulse.enable = true;
+		alsa.enable = true;
 		jack.enable = true;
 	};
 
 	services.logind.settings.Login.HandleLidSwitch = "suspend";
 	services.logind.settings.Login.HandleLidSwitchExternalPower = "suspend";
 
-	environment.etc."inputrc".text = ''
-		set bell-style none
-		'';
-
-	imports =
-		[  
-		./hardware-configuration.nix
-		];
+	environment.etc."inputrc".text = '' set bell-style none '';
+	
+	imports = [ ./hardware-configuration.nix ];
 
 	boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;
@@ -87,13 +77,11 @@
 		LC_TIME = "en_US.UTF-8";
 	};
 
-# Configure keymap in X11
 	services.xserver.xkb = {
 		layout = "us";
 		variant = "";
 	};
 
-# Define a user account. Don't forget to set a password with ‘passwd’.
 	users.users.achka = {
 		isNormalUser = true;
 		description = "achka";
@@ -101,11 +89,9 @@
 		packages = with pkgs; [];
 	};
 
-# Allow unfree packages
 	nixpkgs.config.allowUnfree = true;
 
 	services.xserver.excludePackages = [ pkgs.xterm ];
-
 	services.xserver = { 
 		enable = true;
 
@@ -117,58 +103,56 @@
 		};
 	};
 
-services.displayManager.defaultSession = "none+i3";
-programs.i3lock.enable = true;
+	services.displayManager.defaultSession = "none+i3";
+	programs.i3lock.enable = true;
 
 	environment.systemPackages = with pkgs; [
-#essentials
+		#essentials
 		usbutils
-			nautilus  
-			alacritty
-			brightnessctl
-			pavucontrol
+		nautilus  
+		alacritty
+		brightnessctl
+		pavucontrol
 
-#apps
-			dropbox
-			tmux
-			onlyoffice-desktopeditors
-			kdePackages.okular
-			image-roll
-			emacs
+		#apps
+		dropbox
+		tmux
+		onlyoffice-desktopeditors
+		kdePackages.okular
+		image-roll
+		emacs
 
-#appearance/usability
-			copyq
-			rofi
-			nwg-look
-			papirus-icon-theme
-			dunst
+		#appearance/usability
+		copyq
+		rofi
+		lxappearance
+		papirus-icon-theme
+		dunst
 
-#other essentials (for me)
-			git
-			python3
-			gcc
-			ani-cli
-			bc
-			openjdk21
-			btop
-			playerctl
-			entr
-			bear
-			ripgrep
-			libgccjit
-			nasm
-			gnumake
-			qemu
-			mtools
-			hexedit
-#  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-#  wget
-			];
+		#other essentials (for me)
+		git
+		python3
+		gcc
+		ani-cli
+		bc
+		openjdk21
+		btop
+		playerctl
+		picom
+		entr
+		bear
+		ripgrep
+		libgccjit
+		gnumake
+		qemu
+		mtools
+		hexedit
+	];
 
 	fonts = {
 		enableDefaultPackages = true;
 		packages = with pkgs; [
-			noto-fonts
+				noto-fonts
 				noto-fonts-cjk-sans
 				noto-fonts-color-emoji
 				liberation_ttf
@@ -176,36 +160,18 @@ programs.i3lock.enable = true;
 				fira-code-symbols
 				nerd-fonts.fira-code
 				nerd-fonts.jetbrains-mono
+				nerd-fonts.iosevka
 		];
 	};
 
 	fonts.fontconfig.useEmbeddedBitmaps = true;
 
-# Some programs need SUID wrappers, can be configured further or are
-# started in user sessions.
-# programs.mtr.enable = true;
-# programs.gnupg.agent = {
-#   enable = true;
-#   enableSSHSupport = true;
-# };
+  	services.openssh.enable = true;
 
-# List services that you want to enable:
-
-# Enable the OpenSSH daemon.
-# services.openssh.enable = true;
-
-# Open ports in the firewall.
 	networking.firewall.allowedTCPPorts = [ 22 80 443 17500 4747 ];
 	networking.firewall.allowedUDPPorts = [ 53 17500 4747 ];
-# Or disable the firewall altogether.
 	networking.firewall.enable = true;
 
-# This value determines the NixOS release from which the default
-# settings for stateful data, like file locations and database versions
-# on your system were taken. It‘s perfectly fine and recommended to leave
-# this value at the release version of the first install of this system.
-# Before changing this value read the documentation for this option
-# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	system.stateVersion = "25.11"; # Did you read the comment?
+	system.stateVersion = "25.11";
 
 }
