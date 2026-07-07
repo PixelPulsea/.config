be3e3c54e34075b48cc2b3278e2ec15f5ce850e7
PixelPulse <achka.bt@gmail.com>
2026-07-07 16:32:24 +0800

    Add files via upload

--- CustomScript/GitCloning.sh
+++ CustomScript/GitCloning.sh
@@ -2,48 +2,58 @@
 
 USERNAME="PixelPulsea"
 
-echo "Is it from your own repos? [y/n]"
-read CUSTOM
+read -p "Is it from your own repos? [y/n]: " CUSTOM
 
 if [[ $CUSTOM == "y" || $CUSTOM == "" || $CUSTOM == " " ]]; then
-	echo "Enter the name of the Github Repo"
-	read NAME
-	git clone git@github.com:$USERNAME/$NAME.git
+	read -p "Enter the name of the Github Repo: " NAME
+    read -p  "Enter the branch of your repo: " BRANCH
+	git clone --branch $BRANCH git@github.com:$USERNAME/$NAME.git
 	cd $NAME
 	direnv allow
 elif [[ $CUSTOM == "n" ]]; then
-	echo "Enter the name of the repo from the AUR"
-	read URL
-	git clone https://aur.archlinux.org/$URL.git
+	read -p "Enter the name of the repo: " URL
+	read -p "Is it from AUR? [y/n]: " SOURCE
+	
+	if [[ $SOURCE == "y" ]]; then
+		git clone https://aur.archlinux.org/$URL.git
+		cd $URL
+		read -p "Do you want to install this? (answer 'n' if you're not on arch) [y/n]: " confirmation
 
-	cd $URL
-	echo "Do you want to install this? [y/n]"
-	read confirmation
-
-	if [[ $confirmation == "y" || $confirmation == "" || $confirmation == " " ]]; then
-		echo "===================PKGBUILD==================="
-		cat PKGBUILD
+		if [[ $confirmation == "y" || $confirmation == "" || $confirmation == " " ]]; then
+			echo "===================PKGBUILD==================="
+			cat PKGBUILD
+			echo "=============================================="
+			read -p "Final confirmation. Do you want to install this? [y/n]: " finalconf
+			
+			if [[ $finalconf == "y" || $finalconf == "" || $finalconf == " " ]]; then
+				makepkg -sic -r
+			fi
+		fi
+		
 		echo "=============================================="
-		echo "Final confirmation. Do you want to install this? [y/n]"
-		read finalconf
+		read -p "Remove the source folder? [y/n]: " afterinstall
 
-		if [[ $finalconf == "y" || $finalconf == "" || $finalconf == " " ]]; then
-			makepkg -sic
+		if [[ $afterinstall == "y" || $afterinstall == "" || $afterinstall == " " ]]; then
+			cd ..
+			rm -rf $URL/
+			echo -e "\e[32mCompleted!\e[0m"
+		else
+			echo -e "\e[32mCompleted!\e[0m"
+		fi
+	elif [[ $SOURCE == "n" ]]; then
+		read -p "Enter the username: " USER
+		read -p "Clone with commit/push? [y/n]: " project
+	    branch="main"
+		read -p "Which branch? [press Enter if main]: " branch
+
+		if [[ $project == "y" ]]; then
+			git clone git@github.com:$USER/$URL.git
+		elif [[ $project == "n" ]]; then
+			git clone --branch $branch https://github.com/$USER/$URL
+			echo -e "\e[32mDone!\e[0m"
 		fi
 	fi
-	
-	echo "=============================================="
-	echo "Remove the source folder? [y/n]"
-	read afterinstall
-
-	if [[ $afterinstall == "y" || $afterinstall == "" || $afterinstall == " " ]]; then
-		cd ..
-		rm -rf $URL/
-		echo -e "\e[32mCompleted!\e[0m"
-	else
-		echo -e "\e[32mCompleted!\e[0m"
-	fi
-	
+			 
 else
 	echo "ERROR"
 fi
