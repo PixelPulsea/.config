e29069b3752a67e6fdf50fc524beb147bcbc2486
PixelPulsea <achka.bt@gmail.com>
2026-07-19 13:22:08 +0800

    perfected bar

--- CustomScript/Bars.sh
+++ CustomScript/Bars.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 while true; do
-	#main variables
 	bat_percentage=$(cat /sys/class/power_supply/BAT0/capacity)
 	bat_status=$(cat /sys/class/power_supply/BAT0/status)
-	time=$(date "+%d %B %Y - %I:%M %p")
+	time=$(date "+%d %B %a - %I:%M %p")
 
 	xsetroot -name "$bat_status $bat_percentage% | $time "
 	sleep 60
