7e41a2fb9b3e2174a70208143afc262eaa964922
PixelPulse <achka.bt@gmail.com>
2026-07-22 18:00:51 +0800

    Modify Bars.sh to check for battery presence
    Update battery status check to handle absence of battery.

--- CustomScript/Bars.sh
+++ CustomScript/Bars.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
 while true; do
-	bat_percentage=$(cat /sys/class/power_supply/BAT0/capacity)
-	bat_status=$(cat /sys/class/power_supply/BAT0/status)
 	time=$(date "+%d %B %a - %I:%M %p")
-
-	xsetroot -name "$bat_status $bat_percentage% | $time "
+	bat=$(ls /sys/class/power_supply/)
+	
+	if [[ $bat == "" ]; then
+		xsetroot -name " $time "
+	else
+		bat_percentage=$(cat /sys/class/power_supply/BAT0/capacity)
+		bat_status=$(cat /sys/class/power_supply/BAT0/status)
+		time=$(date "+%d %B %a - %I:%M %p")
+		xsetroot -name "$bat_status $bat_percentage% | $time "
+	fi
+	
 	sleep 60
 done
